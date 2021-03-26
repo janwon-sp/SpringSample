35b7cddbb74b6b8dbaba0d91e282a0c2a9a9e5f5
janwon_k <janwon.kim@sandpit.co.jp>
2021-03-26 15:06:45 +0900

    C:/Program Files/Git/bye ルーティングを別コントローラに追加

--- QuickMaster/src/main/java/to/msn/wings/quickmaster/controllers/GoodByeContloller.java
+++ QuickMaster/src/main/java/to/msn/wings/quickmaster/controllers/GoodByeContloller.java
@@ -4,18 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// コントローラクラスを定義
 @Controller
-public class HelloController {
-
-	// Handlerメソッドを定義
-	@GetMapping("/hello")
+public class GoodByeContloller {
+	@GetMapping("/bye")
 	@ResponseBody
-	public String index() {
-		
-		// 戻り値はレスポンス本体
-		return "こんにちは、世界！！！";
-		
+	public String bye() {
+		return "Bye!!";
 	}
-
 }
