a36ba091fb15b8ea5802ee9d0819f42ec65eaa10
janwon_k <janwon.kim@sandpit.co.jp>
2021-03-26 14:43:31 +0900

    helloコントローラ追加

---
+++ QuickMaster/src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package to.msn.wings.quickmaster.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// コントローラクラスを定義
+@Controller
+public class HelloController {
+
+	// Handlerメソッドを定義
+	@GetMapping("/hello")
+	@ResponseBody
+	public String index() {
+		
+		// 戻り値はレスポンス本体
+		return "こんにちは、世界！！！";
+		
+	}
+}
